c32d29940825a78f25508b89c563085c93ce0837
AbhilashGUI <vemulaabhilash8433@gmail.com>
2025-09-16 09:30:33 +0530

    New code

--- src/test/java/post_put_delete/Post_Put_Delete.java
+++ src/test/java/post_put_delete/Post_Put_Delete.java
@@ -15,7 +15,7 @@ public class Post_Put_Delete {
         @Test
         public void post_request() {
 
-            File file = new File("resources/create_employee.json");
+            File file = new File("create_employee.json");
 
             int id = given()
                     .baseUri("http://dummy.restapiexample.com/api/v1")
@@ -27,7 +27,6 @@ public class Post_Put_Delete {
                     .statusCode(200)
                     .body("data.name", equalTo("Mark"))
                     .extract().path("data.id");
-
             System.out.println(id);
         }
 
@@ -35,9 +34,9 @@ public class Post_Put_Delete {
         public void post_request_using_json_object() {
 
             JSONObject body = new JSONObject();
-            body.put("name", "Tim");
-            body.put("salary", "45678");
-            body.put("age", "35");
+            body.put("name", "Ashton Cox");
+            body.put("salary", "86000");
+            body.put("age", 66);
 
             int id = given()
                     .baseUri("http://dummy.restapiexample.com/api/v1")
@@ -47,7 +46,7 @@ public class Post_Put_Delete {
                     .post("/create").
                     then()
                     .statusCode(200)
-                    .body("data.name", equalTo("Tim"))
+                    .body("data.name", equalTo("Ashton Cox"))
                     .extract().path("data.id");
 
             System.out.println(id);
@@ -57,16 +56,16 @@ public class Post_Put_Delete {
         public void put_request_using_json_object() {
 
             JSONObject body = new JSONObject();
-            body.put("name", "Sam");
-            body.put("salary", "1000");
-            body.put("age", "24");
+            body.put("name", "Tiger Nixon");
+            body.put("salary", "320800");
+            body.put("age", "61");
 
             given()
                     .baseUri("http://dummy.restapiexample.com/api/v1")
                     .contentType(ContentType.JSON)
                     .body(body.toString()).
                     when()
-                    .put("/update/24").
+                    .put("/update/2").
                     then()
                     .statusCode(200);
         }
@@ -78,7 +77,7 @@ public class Post_Put_Delete {
                     .baseUri("http://dummy.restapiexample.com/api/v1")
                     .contentType(ContentType.JSON).
                     when()
-                    .delete("/delete/24").
+                    .delete("/delete/2").
                     then()
                     .statusCode(200)
                     .extract().path("message");
