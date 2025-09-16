6fe113e9fd23e5a1e7e6977edf082dd599a8cf8a
AbhilashGUI <vemulaabhilash8433@gmail.com>
2025-09-16 08:04:49 +0530

    New code

--- src/test/java/getting_started/Getting_Started.java
+++ src/test/java/getting_started/Getting_Started.java
@@ -13,88 +13,86 @@ public class Getting_Started {
     public void simple_get_request() {
 
         given()
-                .baseUri("https://restcountries.eu/rest/v2").
+                .baseUri("https://restcountries.com/v3.1").
                 when()
-                .get("/all").
+                .get("/all?fields=name,flags").
                 then()
                 .statusCode(200);
     }
+        @Test
+        public void validate_json_response() {
 
-    @Test
-    public void validate_json_response() {
+            given()
+                    .baseUri("https://restcountries.com/v3.1").
+                    when()
+                    .get("/alpha/USA").
+                    then()
+                    .statusCode(200)
+                    .body("name.common[0]", equalTo("United States"))
+                    .body("name.official[0]", equalTo("United States of America"))
+                    .body("currencies.USD.symbol[0]", equalTo("$"))
+                    .body("currencies.USD.name[0]", equalTo("United States dollar"))
+                    .body("languages.eng[0]", equalTo("English"));
+        }
 
-        given()
-                .baseUri("https://restcountries.eu/rest/v2").
-                when()
-                .get("/alpha/USA").
-                then()
-                .statusCode(200)
-                .body(
-                        "name", equalTo("United States of America"),
-                        "alpha3Code", equalTo("USA"),
-                        "altSpellings", hasItem("US"),
-                        "currencies[0].symbol", equalTo("$"),
-                        "languages[0].name", equalTo("English")
-                );
+        @Test
+        public void validate_xml_response () {
+            given()
+                    .baseUri("https://api.openweathermap.org/data/2.5")
+                    .queryParam("q", "London,uk")
+                    .queryParam("APPID", "5e76ce9abdf5914f0123cbd6303bafd7")
+                    .queryParam("mode", "xml").
+                    when()
+                    .get("/weather").
+                    then()
+                    .statusCode(200)
+                    .body(
+                            "current.city.@name", equalTo("London"),
+                            "current.city.country", equalTo("GB")
+                    );
+        }
+
+        @Test
+        public void extract_response_data () {
+            Response res = given()
+                    .baseUri("https://restcountries.com/v3.1").
+                    when()
+                    .get("/alpha/USA").
+                    then()
+                    .extract().response();
+            System.out.println(res.asString());
+        }
+
+        @Test
+        public void extract_single_value () {
+            String temperature = given()
+                    .baseUri("https://api.openweathermap.org/data/2.5")
+                    .queryParam("q", "London,uk")
+                    .queryParam("APPID", "5e76ce9abdf5914f0123cbd6303bafd7")
+                    .queryParam("mode", "xml").
+                    when()
+                    .get("/weather").
+                    then()
+                    .statusCode(200)
+                    .body(
+                            "current.city.@name", equalTo("London"),
+                            "current.city.country", equalTo("GB")
+                    )
+                    .extract().path("current.temperature.@value");
+
+            System.out.println(temperature);
+        }
+
+        @Test
+        public void verify_status_line () {
+            given()
+                    .baseUri("https://api.printful.com").
+                    when()
+                    .get("/variant/1").
+                    then()
+                    .statusCode(404)
+                    .statusLine("HTTP/1.1 404 Not Found");
+        }
     }
 
-   @Test
-    public void validate_xml_response() {
-        given()
-                .baseUri("https://api.openweathermap.org/data/2.5")
-                .queryParam("q", "London,uk")
-                .queryParam("APPID", "3f7393c569d314987f3b072d260d7142")
-                .queryParam("mode", "xml").
-                when()
-                .get("/weather").
-                then()
-                .statusCode(200)
-                .body(
-                        "current.city.@name", equalTo("London"),
-                        "current.city.country", equalTo("GB")
-                );
-    }
-
-    @Test
-    public void extract_response_data() {
-        Response res = given()
-                .baseUri("https://restcountries.eu/rest/v2").
-                when()
-                .get("/alpha/USA").
-                then()
-                .extract().response();
-        System.out.println(res.asString());
-    }
-
-    @Test
-    public void extract_single_value() {
-        String temperature = given()
-                .baseUri("https://api.openweathermap.org/data/2.5")
-                .queryParam("q", "London,uk")
-                .queryParam("APPID", "48115954d40e05412d7e3c670f15acb1")
-                .queryParam("mode", "xml").
-                when()
-                .get("/weather").
-                then()
-                .statusCode(200)
-                .body(
-                        "current.city.@name", equalTo("London"),
-                        "current.city.country", equalTo("GB")
-                )
-                .extract().path("current.temperature.@value");
-
-        System.out.println(temperature);
-    }
-
-    @Test
-    public void verify_status_line() {
-        given()
-                .baseUri("https://api.printful.com").
-                when()
-                .get("/variant/1").
-                then()
-                .statusCode(401)
-                .statusLine("HTTP/1.1 401 Unauthorized");
-    }
-}
 
