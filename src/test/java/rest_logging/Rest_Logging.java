169f95185877b95a6070cf7a94c5920c4be20851
AbhilashGUI <vemulaabhilash8433@gmail.com>
2025-09-17 07:20:59 +0530

    New code

--- src/test/java/rest_logging/Rest_Logging.java
+++ src/test/java/rest_logging/Rest_Logging.java
@@ -9,53 +9,53 @@ public class Rest_Logging {
     public void log_all_details() {
 
         given()
-                .baseUri("https://restcountries.eu/rest/v2").
+                .baseUri("https://restcountries.com/v3.1").
                 when()
                 .get("/alpha/US").
                 then()
-                //.log().all()
-                .log().everything();
+                .log().all();
+
 
     }
 
     @Test
     public void log_body_or_header() {
         given()
-                .baseUri("https://restcountries.eu/rest/v2").
-                when()
+                .baseUri("https://restcountries.com/v3.1")
+                .when()
                 .get("/alpha/US").
                 then()
-                //.log().headers();
-                .log().body();
+                .log().headers();
+                //.log().body();
     }
 
     @Test
     public void log_cookies_or_status() {
         given()
-                .baseUri("https://restcountries.eu/rest/v2").
-                when()
+                .baseUri("https://restcountries.com/v3.1")
+                .when()
                 .get("/alpha/US").
                 then()
-                //.log().status()
-                .log().cookies();
+                .log().status();
+        //.log().cookies();
     }
 
     @Test
     public void log_if_error() {
         given()
-                .baseUri("https://restcountries.eu/rest/v2").
+                .baseUri("https://restcountries.com/v3.1").
                 when()
                 .get("/alpha/UT").
                 then()
                 //.log().ifStatusCodeIsEqualTo(404)
                 .log().ifError()
-                .statusCode(200);
+                .statusCode(404);
     }
 
     @Test
     public void log_if_validation_fails() {
         given()
-                .baseUri("https://restcountries.eu/rest/v2").
+                .baseUri("https://restcountries.com/v3.1").
                 when()
                 .get("/alpha/UT").
                 then()
